0328def3e3526f7b65492bf42263c05542db932d
agis <agis.septiyanto@gmail.com>
2024-01-13 21:22:27 +0700

    remove unused style

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,8 +1,4 @@
 <x-app-layout>
-    <style>
-        [x-cloak] {
-            display: none !important;
-        }    </style>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Board ' . $board->name) }}
